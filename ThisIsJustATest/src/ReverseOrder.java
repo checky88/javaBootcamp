86602b316cd43cd0ab9a16f692d41f42504f9fa7
KellyR <KellyR@LAPTOP1335.V1.com>
2017-02-14 13:33:49 +0000

    ReverseOrder issue

---
+++ ThisIsJustATest/src/ReverseOrder.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseOrder {
+
+	public static void main(String[] args) {
+		
+		String s = "A sample sentence is required";
+		
+		List<String> myList = new ArrayList<String>(Arrays.asList(s.split(" ")));
+		
+		Collections.reverse(myList);
+		System.out.println("After Reverse Order, ArrayList Contains : " + myList);
+		
+		
+
+	}
+
+}
